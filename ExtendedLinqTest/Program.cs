687a37274488dc725f2c87a7898cb76a5f68966a
zquanghoangz <zquanghoangz@gmail.com>
2016-10-07 12:11:21 +0800

    Create solution

--- ExtendedLinqTest/Program.cs
+++ ExtendedLinqTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExtendedLinq;
 
 namespace ExtendedLinqTest
 {
@@ -10,6 +11,15 @@ namespace ExtendedLinqTest
     {
         static void Main(string[] args)
         {
+            var input = "Dog,Cat,Rabbit,Dog,Dog,Lizard,Cat,Cat,Dog,Rabbit,Guinea Pig,Dog";
+            var output = input.Split(',').CountDuplicated();
+
+            foreach (var it in output)
+            {
+                Console.WriteLine("{0} : {1}", it.Key, it.Value);
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
